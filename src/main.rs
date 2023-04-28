cf7398866c7efb63c8c9104812bf785516b68eb6
wwagner4 <wwagner4@gmail.com>
2023-04-28 07:15:07 +0200

    sleep without await
    (cherry picked from commit aba842d92fe2abd7243ebaddd21e43be937d45b1)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
+use tokio::time::*;
+
 
 #[tokio::main]
 async fn main() {
     println!("start program");
-    af().await;
+    af(23).await;
+    af(24).await;
+    af(25).await;
+    af(26).await;
     println!("finish program");
 }
 
-async fn af() {
-    println!("executing inside async");
+async fn af(num: u32) {
+    let d = Duration::from_millis(1000);
+    sleep(d);
+    println!("executing async function {num} slept {d:?}ms");
 } 
