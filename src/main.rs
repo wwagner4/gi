cca1f98d9ece5f663f8fd4e17a0d2607b735f5b5
wwagner4 <wwagner4@gmail.com>
2023-04-28 11:35:57 +0200

    running concurren and passing borrowed parameter
    (cherry picked from commit f6b4e2b81fe25ed951a8faac32a94f37167360da)

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,40 @@
 use tokio::time::{Duration, sleep};
 use rand::random;
+use std::sync::Arc;
 
 
 #[tokio::main]
 async fn main() {
     println!("start program");
+    let str2 = "hallo...".to_string();
+    afs(&str2).await;
+    println!("finish program");
+}
+
+async fn afs(str2: &str) {
     let vs = (0..10).collect::<Vec<i32>>();
     let mut hs = vec![];
+    let str2_arc = Arc::new(str2.to_owned());
     for v in vs {
-        let h = tokio::spawn(af(v));
-        hs.push(h)
+        let h = tokio::spawn({
+            let str2_arc = Arc::clone(&str2_arc);
+            async move {
+                af(v, &str2_arc).await;
+            }
+        });
+        hs.push(h);
     }
 
     for h in hs {
         h.await.unwrap();
     }
-    println!("finish program");
 }
 
-async fn af(num: i32) {
+async fn af(num: i32, str3: &Arc<String>) {
+    println!("started async function {num}");
     let d1 = random::<u64>() % 900 + 100;
     let d = Duration::from_millis(d1);
     sleep(d).await;
-    println!("executing async function {num} slept {d:?}");
+    let s3 = str3.as_str();
+    println!("finished async function {num} |{s3}| slept {d:?}");
 } 
