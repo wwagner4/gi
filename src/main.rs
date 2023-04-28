79ea6df1cd112d5121a19808e85749907a2bddc7
wwagner4 <wwagner4@gmail.com>
2023-04-28 11:58:08 +0200

    running concurren with and without arc

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,27 @@ use std::sync::Arc;
 #[tokio::main]
 async fn main() {
     println!("start program");
-    let str2 = "hallo...".to_string();
-    afs(&str2).await;
+    afs_borrowed().await;
+    afs_not_borrowed().await;
     println!("finish program");
 }
 
-async fn afs(str2: &str) {
+async fn my_async_function(num: i32, str3: &str) {
+    println!("started async function {num}");
+    let d1 = random::<u64>() % 900 + 100;
+    let d = Duration::from_millis(d1);
+    sleep(d).await;
+    println!("finished async function {num} |{str3}| slept {d:?}");
+} 
+
+async fn afs_not_borrowed() {
+    let str1 = "somehow not borrowed string";
     let vs = (0..10).collect::<Vec<i32>>();
-    let mut hs = vec![];
-    let str2_arc = Arc::new(str2.to_owned());
+    let mut hs = vec!();
     for v in vs {
         let h = tokio::spawn({
-            let str2_arc = Arc::clone(&str2_arc);
             async move {
-                af(v, &str2_arc).await;
+                my_async_function(v, &str1).await;
             }
         });
         hs.push(h);
@@ -30,11 +37,26 @@ async fn afs(str2: &str) {
     }
 }
 
-async fn af(num: i32, str3: &Arc<String>) {
-    println!("started async function {num}");
-    let d1 = random::<u64>() % 900 + 100;
-    let d = Duration::from_millis(d1);
-    sleep(d).await;
-    let s3 = str3.as_str();
-    println!("finished async function {num} |{s3}| slept {d:?}");
-} 
+async fn afs_borrowed() {
+    let str1 = "borrowed string (Arc hell)".to_string();
+    let str_arc1 = Arc::new(str1);
+
+    let vs = (0..10).collect::<Vec<i32>>();
+    let mut hs = vec!();
+    for v in vs {
+        let h = tokio::spawn({
+            let str_arc2 = Arc::clone(&str_arc1);
+            async move {
+                async {
+                    let x = str_arc2.as_str();
+                    my_async_function(v, x).await 
+                }.await
+            }
+        });
+        hs.push(h);
+    }
+
+    for h in hs {
+        h.await.unwrap();
+    }
+}
