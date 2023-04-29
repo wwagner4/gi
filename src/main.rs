cb4f3569ddd60af79a857809383e5346d871ed9e
wwagner4 <wwagner4@gmail.com>
2023-04-29 19:21:34 +0200

    streams with data struct

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,51 @@ use futures::stream;
 
 #[tokio::main]
 async fn main() {
-    println!("---> main");
+    println!("-> main");
     with_stream().await;
-    println!("<--- main");
+    println!("<- main");
+}
+
+struct MyData {
+    id: u8,
+    name: String,
+    lang: Vec<String>,
+}
+
+impl MyData {
+    fn create(id: u8, name: &str, lang: &Vec<String>) -> Self {
+        MyData { id: id, name: name.to_string(), lang: lang.clone() }
+    }
+}
+
+fn create_datas() -> Vec<MyData> {
+    let lang = vec!("DE".to_string(), "FR".to_string(), "ES".to_string());
+    vec!(
+        MyData::create(0, "erwin", &lang),
+        MyData::create(1, "fromm", &lang),
+        MyData::create(2, "leben", &lang),
+        MyData::create(3, "liebe", &lang),
+        MyData::create(4, "lebendig", &lang),
+    )
 }
 
 async fn with_stream() {
-    println!("---> with stream");
-    let str6 = "not borrowed";
-    let nums = (0..15).collect::<Vec<i32>>();
-    stream::iter(nums)
+    println!("--> with stream");
+    let datas = create_datas();
+    stream::iter(datas)
         .for_each_concurrent(
-            Some(10), |value| async move {
-                my_async_function(value, &str6).await
+            Some(30), |value| async move {
+                my_async_function(&value).await
             },
         ).await;
-    println!("<--- with stream");
+    println!("<-- with stream");
 }
 
-async fn my_async_function(num: i32, str3: &str) -> () {
-    println!("---> async function {num}");
-    let d1 = random::<u64>() % 3000 + 10;
+async fn my_async_function(data: &MyData) -> () {
+    println!("---> async function {} |{}| {:?}", data.id, data.name, data.lang);
+    let d1 = random::<u64>() % 1000 + 10;
     let d = Duration::from_millis(d1);
     sleep(d).await;
-    println!("<--- async function {num} |{str3}| slept {d:?}");
+    println!("<--- async function {} |{}| {:?} slept {:?}", data.id, data.name, data.lang, d);
 }
 
