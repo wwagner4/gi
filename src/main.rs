736e47029c3324fadae41bf363cd487275de237f
wwagner4 <wwagner4@gmail.com>
2023-04-30 08:05:19 +0200

    simplified

--- src/main.rs
+++ src/main.rs
@@ -13,22 +13,21 @@ async fn main() {
 
 async fn with_stream() {
     println!("---> with stream");
-    let str6 = "not borrowed";
     let nums = (0..15).collect::<Vec<i32>>();
     stream::iter(nums)
         .for_each_concurrent(
             Some(10), |value| async move {
-                my_async_function(value, &str6).await
+                my_async_function(value).await
             },
         ).await;
     println!("<--- with stream");
 }
 
-async fn my_async_function(num: i32, str3: &str) -> () {
+async fn my_async_function(num: i32) -> () {
     println!("---> async function {num}");
     let d1 = random::<u64>() % 3000 + 10;
     let d = Duration::from_millis(d1);
     sleep(d).await;
-    println!("<--- async function {num} |{str3}| slept {d:?}");
+    println!("<--- async function {num} slept {d:?}");
 }
 
