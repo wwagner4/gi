3c6477ce711fd31324038ffdd4668b9753e9985b
wwagner4 <wwagner4@gmail.com>
2023-05-01 18:13:54 +0200

    works with struct parameter result

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,20 @@ use rand::random;
 use futures::TryStreamExt;
 use futures::stream;
 
+#[derive(Debug)]
+struct ForcedTestErr;
+
+struct MyData {
+    id: u8,
+    name: String,
+    lang: Vec<String>,
+}
+
+impl MyData {
+    fn create(id: u8, name: &str, lang: &Vec<String>) -> Self {
+        MyData { id: id, name: name.to_string(), lang: lang.clone() }
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -11,36 +25,45 @@ async fn main() {
     println!("<--- main");
 }
 
+fn create_datas() -> Vec<MyData> {
+    let lang = vec!("DE".to_string(), "FR".to_string(), "ES".to_string());
+    vec!(
+        MyData::create(0, "erwin", &lang),
+        MyData::create(1, "fromm", &lang),
+        MyData::create(2, "leben", &lang),
+        MyData::create(3, "liebe", &lang),
+        MyData::create(4, "lebendig", &lang),
+    )
+}
+
 async fn with_stream() {
     println!("---> with stream");
-    let result_nums  = (0..15)
-        .into_iter()
-        .map(|x|  Ok(x)  )
-        .collect::<Vec<Result<i32, _>>>();
+    let datas = &create_datas();
+    let result_nums = datas
+        .iter()
+        .map(|x| Ok(x))
+        .collect::<Vec<Result<_, _>>>();
     let result_all = stream::iter(result_nums)
         .try_for_each_concurrent(
-            None, |value| async move {
-                my_async_function(&value).await
-            },
+            Some(3),
+            |value| async move { my_async_function(&value).await },
         ).await;
     match result_all {
-        Ok(_) => println!("concurrent execution was successful"),
-        Err(e) => println!("concurrent execution resulted in Error: {:?}", e),
+        Ok(_) => println!("# concurrent execution of {} data was successful", datas.len()),
+        Err(e) => println!("# concurrent execution resulted in Error: {:?}", e),
     }
     println!("<--- with stream");
 }
 
-#[derive(Debug)]
-struct WantedErr;
-
-async fn my_async_function(num: &i32) -> Result<(), WantedErr>{
-    println!("---> async function result {}", num);
-    let d1 = random::<u64>() % 1000 + 10;
-    if d1 < 100 {
-        return Err(WantedErr);
+async fn my_async_function(data: &MyData) -> Result<(), ForcedTestErr> {
+    println!("---> async function result {} {} {:?}", data.id, data.name, data.lang);
+    let ran_num = random::<u64>() % 1000;
+    if ran_num < 100 {
+        return Err(ForcedTestErr);
     }
-    let d = Duration::from_millis(d1);
-    sleep(d).await;
-    println!("<--- async function result {} slept {:?}", num, d);
+    let dur = Duration::from_millis(ran_num + 10);
+    sleep(dur).await;
+    println!("<--- async function result {} slept {:?}", data.id, dur);
     Ok(())
 }
+
