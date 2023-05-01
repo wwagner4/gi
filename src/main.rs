b3abad7f544f28baf707c9a5b5ee971d013bea5b
wwagner4 <wwagner4@gmail.com>
2023-05-01 07:58:36 +0200

    works with simple async result function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use tokio::time::{Duration, sleep};
 use rand::random;
-use futures::{StreamExt};
+use futures::TryStreamExt;
 use futures::stream;
 
 
@@ -13,29 +13,34 @@ async fn main() {
 
 async fn with_stream() {
     println!("---> with stream");
-    let nums = (0..15).collect::<Vec<i32>>();
-    stream::iter(nums)
-        .for_each_concurrent(
-            Some(10), |value| async move {
-                my_async_function(value).await
+    let result_nums  = (0..15)
+        .into_iter()
+        .map(|x|  Ok(x)  )
+        .collect::<Vec<Result<i32, _>>>();
+    let result_all = stream::iter(result_nums)
+        .try_for_each_concurrent(
+            None, |value| async move {
+                my_async_function(&value).await
             },
         ).await;
+    match result_all {
+        Ok(_) => println!("concurrent execution was successful"),
+        Err(e) => println!("concurrent execution resulted in Error: {:?}", e),
+    }
     println!("<--- with stream");
 }
 
-async fn my_async_function(num: i32) -> () {
-    println!("---> async function {num}");
-    let d1 = random::<u64>() % 3000 + 10;
-    let d = Duration::from_millis(d1);
-    sleep(d).await;
-    println!("<--- async function {num} slept {d:?}");
-}
+#[derive(Debug)]
+struct WantedErr;
 
-async fn my_async_function1(num: i32) -> anyhow::Result<()>{
+async fn my_async_function(num: &i32) -> Result<(), WantedErr>{
     println!("---> async function result {}", num);
     let d1 = random::<u64>() % 1000 + 10;
+    if d1 < 100 {
+        return Err(WantedErr);
+    }
     let d = Duration::from_millis(d1);
     sleep(d).await;
-    println!("<--- async function result {}slept {:?}", num, d);
+    println!("<--- async function result {} slept {:?}", num, d);
     Ok(())
 }
